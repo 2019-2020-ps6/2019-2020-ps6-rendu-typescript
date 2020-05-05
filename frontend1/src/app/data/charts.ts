3a067309f978a33b8e7bf6933e5a16084717e708
Lemuel-DONTO <55319038+Lemouz-NALMOK@users.noreply.github.com>
2020-05-05 07:29:26 +0200

    Last Summary

--- frontend1/src/app/data/charts.ts
+++ frontend1/src/app/data/charts.ts
@@ -1,10 +1,10 @@
 import { Colors } from '../constants/colors.service';
 export const lineChartData = {
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+  labels: [],
   datasets: [
     {
       label: '',
-      data: [54, 63, 60, 65, 60, 68, 60],
+      data: [],
       borderColor: Colors.getColors().themeColor1,
       pointBackgroundColor: Colors.getColors().foregroundColor,
       pointBorderColor: Colors.getColors().themeColor1,
